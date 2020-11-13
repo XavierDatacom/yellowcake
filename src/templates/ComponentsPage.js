bb625a645819e1550ce12646d6ac981990d1e6d2
XavierDatacom <74297197+XavierDatacom@users.noreply.github.com>
2020-11-13 11:23:29 +0100

    Update ComponentsPage.js

--- src/templates/ComponentsPage.js
+++ src/templates/ComponentsPage.js
@@ -48,11 +48,11 @@ export const ComponentsPageTemplate = ({
       </div>
     </section>
 
-    <section className="BackgroundVideo-section section">
+    /*<section className="BackgroundVideo-section section">
       <BackgroundVideo poster={videoPoster} videoTitle={videoTitle}>
         {video && <source src={video} type="video/mp4" />}
       </BackgroundVideo>
-    </section>
+    </section>*/
 
     <section className="section">
       <div className="container">
